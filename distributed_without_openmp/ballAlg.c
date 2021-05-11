dfa1a42b0d8474e6c309682e989c4ab06dbb0a11
joaomfigueiredo <joao.m.figueiredo@tecnico.ulisboa.pt>
2021-05-11 18:41:33 +0100

    mpi theoric

--- distributed_without_openmp/ballAlg.c
+++ distributed_without_openmp/ballAlg.c
@@ -197,22 +197,41 @@ int main(int argc, char *argv[])
     node* tree;
 
     //____________START_TIME_BENCHMARK_____________ 
+    //----------Não sei o que fazer com isto
     exec_time = -omp_get_wtime(); 
     MPI_Init(&argc, &argv);
 
-    //generates dataset
-    pts = get_points(argc, argv);
+    if(rank == 0)
+    {
+        //generates dataset
+        //-----------------Por enquanto vamos deixar só um proc receber os pontos e enviar para os outros
+        pts = get_points(argc, argv);
 
-    double* pts_first_position = pts[0]; //position of the first element of pts (pts is sorted so it would be lost and hard to free)
-    int n_dims = atoi(argv[1]); //number of dimensions 
-    long n_points = atoi(argv[2]); //number of points in the set
-    double* projections = (double*)malloc(n_points*sizeof(double)); //array to store pseudo-projections the locate the point in the line 
-    long n_nodes = 2 * n_points - 1; //number of nodes in the tree 
-    tree = (node*)malloc(n_nodes*sizeof(node));
-    
-    // construct THE TREE
-    build_tree(tree, 0, pts, projections, n_points, n_dims);
+        double* pts_first_position = pts[0]; //position of the first element of pts (pts is sorted so it would be lost and hard to free)
+        int n_dims = atoi(argv[1]); //number of dimensions 
+        long n_points = atoi(argv[2]); //number of points in the set
+        double* projections = (double*)malloc(n_points*sizeof(double)); //array to store pseudo-projections the locate the point in the line 
+        long n_nodes = 2 * n_points - 1; //number of nodes in the tree 
+        tree = (node*)malloc(n_nodes*sizeof(node));
 
+        // construct THE TREE
+
+        
+        Odd_ranks={1, 3, 5}, Even_ranks={0, 2, 4}
+
+        MPI_Comm_group(MPI_COMM_WORLD, Old_group)
+
+        MPI_Group_incl(Old_group, 3, Odd_ranks, &Odd_group)
+
+        MPI_Group_incl(Old_group, 3, Even_ranks, &Even_group)
+
+        MPI_Comm_create(MPI_COMM_WORLD, Odd_group, Odd_Comm)
+
+        MPI_Comm_create(MPI_COMM_WORLD, Even_group, Even_Comm) 
+
+        build_tree(tree, 0, pts, projections, n_points, n_dims, Odd_Comm); 10
+        build_tree(tree, 0, pts, projections, n_points, n_dims, Even_Comm); 10
+    }
 
     MPI_Finalize();
     //____________END_TIME_BENCHMARK_____________
